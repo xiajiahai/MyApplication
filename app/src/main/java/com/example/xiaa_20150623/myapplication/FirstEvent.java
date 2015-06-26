9485485160d640983ee20a4c0cd07492db8601a0
xiajiahai <xia_jia_hai@aliyun.com>
2015-06-26 16:08:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiaa_20150623/myapplication/FirstEvent.java
@@ -0,0 +1,13 @@
+package com.example.xiaa_20150623.myapplication;
+
+public class FirstEvent {
+
+    private String mMsg;
+    public FirstEvent(String msg) {
+        // TODO Auto-generated constructor stub
+        mMsg = msg;
+    }
+    public String getMsg(){
+        return mMsg;
+    }
+}
---
+++ app/src/main/java/com/example/xiaa_20150623/myapplication/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.example.xiaa_20150623.myapplication;
+
+
+import com.example.xiaa_20150623.myapplication.FirstEvent;
+
+import de.greenrobot.event.EventBus;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends Activity {
+    private Button btn_FirstEvent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        btn_FirstEvent = (Button) findViewById(R.id.btn_first_event);
+
+        btn_FirstEvent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                EventBus.getDefault().post(
+                        new FirstEvent("FirstEvent btn clicked"));
+            }
+        });
+    }
+}
